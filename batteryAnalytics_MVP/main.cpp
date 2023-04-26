a896c4ac1f4c9f37beaf6ddb8bf279df66a328ef
yaguts1 <64816747+yaguts1@users.noreply.github.com>
2023-04-26 20:26:53 -0300

    Combo menu buf fixed

--- batteryAnalytics_MVP/main.cpp
+++ batteryAnalytics_MVP/main.cpp
@@ -16,6 +16,7 @@ int main()
 	// Make button corners rounded
 	float rounding = 10.0f; // Adjust to desired corner rounding
 	ImGui::PushStyleVar(ImGuiStyleVar_FrameRounding, rounding);
+
 	while (window.isOpen())
 	{
 		sf::Event event;
@@ -37,84 +38,95 @@ int main()
 		ImGui::SFML::Update(window, clock.restart());
 		//Start adding gui elements
 		ImGui::Begin("Location", nullptr, ImGuiWindowFlags_NoMove | ImGuiWindowFlags_NoResize);
+		
 
+		const char* items_states[] = { "SP", "TO", "GO" };
+
+		const char* items_cities_SP[] = { "Sao Paulo", "Rio Claro", "Campos de Jordao","Adamantina" };
+		const char* items_cities_TO[] = { "Palmas", "Gurupi", "Porto Nacional" };
+		const char* items_cities_GO[] = { "Goiania", "Anapolis", "Catalao" };
+
+		static int current_state = 0;
+		static int current_city = 0;
 		ImGui::SetWindowFontScale(1.4);
 		ImGui::SetWindowPos(ImVec2(0.5 * (WINDOW_WIDTH / 12.0f), WINDOW_HEIGHT / 5.0f));
-		ImGui::SetWindowSize(ImVec2(3*(WINDOW_WIDTH /10.0f), 2.0 * (WINDOW_HEIGHT / 10.0f)));
-		const char* items_states[] = { "SP", "TO", "GO" };
-		static int currentState = 0;
-		//ImGui::SetCursorPos(ImVec2(ImGui::GetCursorPos().x + 0.6 * (WINDOW_WIDTH / 10.0f), ImGui::GetCursorPos().y + 0.20 * (WINDOW_HEIGHT / 10.0f)));
+		ImGui::SetWindowSize(ImVec2(3 * (WINDOW_WIDTH / 10.0f), 2.0 * (WINDOW_HEIGHT / 10.0f)));
+		ImGui::SetCursorPos(ImVec2(ImGui::GetCursorPos().x + 0.6 * (WINDOW_WIDTH / 10.0f), ImGui::GetCursorPos().y + 0.20 * (WINDOW_HEIGHT / 10.0f)));
 		ImGui::Text("UF");
 		ImGui::SetNextItemWidth(200);
-		//ImGui::SetCursorPos(ImVec2(ImGui::GetCursorPos().x + 0.1 * (WINDOW_WIDTH / 10.0f), ImGui::GetCursorPos().y));
-		 ImGui::Combo(" ", &currentState, items_states, IM_ARRAYSIZE(items_states));
-		switch (currentState)
-		{	
-			case 0:
-			{
-				const char* items_cities_SP[] = { "Sao Paulo", "Rio Claro", "Campos de Jordao","Adamantina" };
-				static int currentCity = 0;
-				//ImGui::SetCursorPos(ImVec2(ImGui::GetCursorPos().x + 0.55 * (WINDOW_WIDTH / 10.0f), ImGui::GetCursorPos().y + 0.20 * (WINDOW_HEIGHT / 10.0f)));
-				ImGui::Text("City");
-				//ImGui::SetCursorPos(ImVec2(ImGui::GetCursorPos().x + 0.1 * (WINDOW_WIDTH / 10.0f), ImGui::GetCursorPos().y));
-				ImGui::SetNextItemWidth(200);
-				ImGui::Combo(" ", &currentCity, items_cities_SP, IM_ARRAYSIZE(items_cities_SP));
-				//ImGui::SetCursorPos(ImVec2(ImGui::GetCursorPos().x + 1.6 * (WINDOW_WIDTH / 10.0f), ImGui::GetCursorPos().y - 1 * (WINDOW_HEIGHT / 10.0f)));
-				ImGui::Text("Power distribution \ncompany");
-				switch (currentCity)
-				{
-					case 0:
-					{
-						ImGui::SetWindowFontScale(2.0);
-						ImGui::SetCursorPos(ImVec2(ImGui::GetCursorPos().x + 1.6 * (WINDOW_WIDTH / 10.0f), ImGui::GetCursorPos().y + 0.15 * (WINDOW_HEIGHT / 10.0f)));
-						ImGui::Text("ENEL SP");
-						ImGui::SetWindowFontScale(1.4);
-						break;
-					}
-					case 1:
-					{
-						ImGui::SetWindowFontScale(2.0);
-						ImGui::SetCursorPos(ImVec2(ImGui::GetCursorPos().x + 1.6 * (WINDOW_WIDTH / 10.0f), ImGui::GetCursorPos().y + 0.15 * (WINDOW_HEIGHT / 10.0f)));
-						ImGui::Text("ELEKTRO");
-						ImGui::SetWindowFontScale(1.4);
-						break;
-					}
-					case 2:
-					{
-						ImGui::SetWindowFontScale(2.0);
-						ImGui::SetCursorPos(ImVec2(ImGui::GetCursorPos().x + 1.6 * (WINDOW_WIDTH / 10.0f), ImGui::GetCursorPos().y + 0.15 * (WINDOW_HEIGHT / 10.0f)));
-						ImGui::Text("ELEKTRO");
-						ImGui::SetWindowFontScale(1.4);
-						break;
-					}
-					case 3:
-					{
-						ImGui::SetWindowFontScale(2.0);
-						ImGui::SetCursorPos(ImVec2(ImGui::GetCursorPos().x + 1.6 * (WINDOW_WIDTH / 10.0f), ImGui::GetCursorPos().y + 0.15 * (WINDOW_HEIGHT / 10.0f)));
-						ImGui::Text("ELEKTRO");
-						ImGui::SetWindowFontScale(1.4);
-						break;
-					}
-					default:
-						break;
-				}
-				break;
-			}
+		ImGui::SetCursorPos(ImVec2(ImGui::GetCursorPos().x + 0.1 * (WINDOW_WIDTH / 10.0f), ImGui::GetCursorPos().y));
+		ImGui::Combo("State", &current_state, items_states, IM_ARRAYSIZE(items_states));
 
-			case 1:
-			{
+		switch (current_state){
+		case 0: // SP
+			ImGui::SetCursorPos(ImVec2(ImGui::GetCursorPos().x + 0.55 * (WINDOW_WIDTH / 10.0f), ImGui::GetCursorPos().y + 0.20 * (WINDOW_HEIGHT / 10.0f)));
+			ImGui::Text("City");
+			ImGui::SetCursorPos(ImVec2(ImGui::GetCursorPos().x + 0.1 * (WINDOW_WIDTH / 10.0f), ImGui::GetCursorPos().y));
+			ImGui::SetNextItemWidth(200);
+			ImGui::Combo("City", &current_city, items_cities_SP, IM_ARRAYSIZE(items_cities_SP));
+			ImGui::SetCursorPos(ImVec2(ImGui::GetCursorPos().x + 1.6 * (WINDOW_WIDTH / 10.0f), ImGui::GetCursorPos().y - 1 * (WINDOW_HEIGHT / 10.0f)));
+			ImGui::Text("Power distribution \ncompany");
+			switch (current_city) {
+			case 0: // Sao Paulo
+				// do something for Sao Paulo in SP
+				ImGui::SetWindowFontScale(2.0);
+				ImGui::SetCursorPos(ImVec2(ImGui::GetCursorPos().x + 1.6 * (WINDOW_WIDTH / 10.0f), ImGui::GetCursorPos().y + 0.15 * (WINDOW_HEIGHT / 10.0f)));
+				ImGui::Text("ENEL SP");
+				ImGui::SetWindowFontScale(1.4);
+				break;
+			case 1: // Rio Claro
+				// do something for Rio Claro in SP
+				ImGui::SetWindowFontScale(2.0);
+				ImGui::SetCursorPos(ImVec2(ImGui::GetCursorPos().x + 1.6 * (WINDOW_WIDTH / 10.0f), ImGui::GetCursorPos().y + 0.15 * (WINDOW_HEIGHT / 10.0f)));
+				ImGui::Text("ELEKTRO");
+				ImGui::SetWindowFontScale(1.4);
+				break;
+			case 2: // Campos de Jordao
+				// do something for Campos de Jordao in SP
+				ImGui::SetWindowFontScale(2.0);
+				ImGui::SetCursorPos(ImVec2(ImGui::GetCursorPos().x + 1.6 * (WINDOW_WIDTH / 10.0f), ImGui::GetCursorPos().y + 0.15 * (WINDOW_HEIGHT / 10.0f)));
+				ImGui::Text("ELEKTRO");
+				ImGui::SetWindowFontScale(1.4);
 
 				break;
-			}
-			case 2:
-			{
-
-			}
-			default:
+			case 3: // Adamantina
+				// do something for Adamantina in SP
+				ImGui::SetWindowFontScale(2.0);
+				ImGui::SetCursorPos(ImVec2(ImGui::GetCursorPos().x + 1.6 * (WINDOW_WIDTH / 10.0f), ImGui::GetCursorPos().y + 0.15 * (WINDOW_HEIGHT / 10.0f)));
+				ImGui::Text("ELEKTRO");
+				ImGui::SetWindowFontScale(1.4);
 				break;
-		
+			}
+			break;
+		case 1: // TO
+			ImGui::Combo("City", &current_city, items_cities_TO, IM_ARRAYSIZE(items_cities_TO));
+			switch (current_city) {
+			case 0: // Palmas
+				// do something for Palmas in TO
+				break;
+			case 1: // Gurupi
+				// do something for Gurupi in TO
+				break;
+			case 2: // Porto Nacional
+				// do something for Porto Nacional in TO
+				break;
+			}
+			break;
+		case 2: // GO
+			ImGui::Combo("City", &current_city, items_cities_GO, IM_ARRAYSIZE(items_cities_GO));
+			switch (current_city) {
+			case 0: // Goiania
+				// do something for Goiania in GO
+				break;
+			case 1: // Anapolis
+				// do something for Anapolis in GO
+				break;
+			case 2: // Catalao
+				// do something for Catalao in GO
+				break;
+			}
+			break;
 		}
-
 		ImGui::End();
 
 		//Finished adding gui elements 
