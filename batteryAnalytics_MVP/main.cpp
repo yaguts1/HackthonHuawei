d89ecb6b1c7ba2d4bb06f936f7608c3dd9b9c4f5
yaguts1 <64816747+yaguts1@users.noreply.github.com>
2023-04-26 21:36:30 -0300

    Added a few cities and organized the TUSD text

--- batteryAnalytics_MVP/main.cpp
+++ batteryAnalytics_MVP/main.cpp
@@ -37,7 +37,7 @@ int main()
 
 		ImGui::SFML::Update(window, clock.restart());
 		//Start adding gui elements
-		ImGui::Begin("Location", nullptr, ImGuiWindowFlags_NoMove | ImGuiWindowFlags_NoResize);
+		ImGui::Begin("Location", nullptr, ImGuiWindowFlags_NoMove | ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoCollapse);
 
 
 		const char* items_states[] = { "SP", "TO", "GO" };
@@ -52,13 +52,13 @@ int main()
 		ImGui::SetWindowPos(ImVec2(0.5 * (WINDOW_WIDTH / 12.0f), WINDOW_HEIGHT / 5.0f));
 		ImGui::SetWindowSize(ImVec2(3 * (WINDOW_WIDTH / 10.0f), 2.0 * (WINDOW_HEIGHT / 10.0f)));
 		ImGui::SetCursorPos(ImVec2(ImGui::GetCursorPos().x + 0.6 * (WINDOW_WIDTH / 10.0f), ImGui::GetCursorPos().y + 0.20 * (WINDOW_HEIGHT / 10.0f)));
-		ImGui::Text("UF");
+		ImGui::Text("");
 		ImGui::SetNextItemWidth(200);
 		ImGui::SetCursorPos(ImVec2(ImGui::GetCursorPos().x + 0.1 * (WINDOW_WIDTH / 10.0f), ImGui::GetCursorPos().y));
 		ImGui::Combo("State", &current_state, items_states, IM_ARRAYSIZE(items_states));
 
 		ImGui::SetCursorPos(ImVec2(ImGui::GetCursorPos().x + 0.55 * (WINDOW_WIDTH / 10.0f), ImGui::GetCursorPos().y + 0.20 * (WINDOW_HEIGHT / 10.0f)));
-		ImGui::Text("City");
+		ImGui::Text("");
 		ImGui::SetCursorPos(ImVec2(ImGui::GetCursorPos().x + 0.1 * (WINDOW_WIDTH / 10.0f), ImGui::GetCursorPos().y));
 		ImGui::SetNextItemWidth(200);
 
@@ -66,51 +66,61 @@ int main()
 		case 0: // SP
 
 			ImGui::Combo("City", &current_city, items_cities_SP, IM_ARRAYSIZE(items_cities_SP));
-			ImGui::SetCursorPos(ImVec2(ImGui::GetCursorPos().x + 1.6 * (WINDOW_WIDTH / 10.0f), ImGui::GetCursorPos().y - 1 * (WINDOW_HEIGHT / 10.0f)));
+			ImGui::SetCursorPos(ImVec2(ImGui::GetCursorPos().x + 1.8 * (WINDOW_WIDTH / 10.0f), ImGui::GetCursorPos().y - 1 * (WINDOW_HEIGHT / 10.0f)));
 			ImGui::Text("Power distribution \ncompany");
 			switch (current_city) {
 			case 0: // Sao Paulo
 				// do something for Sao Paulo in SP
 				ImGui::SetWindowFontScale(2.0);
-				ImGui::SetCursorPos(ImVec2(ImGui::GetCursorPos().x + 1.6 * (WINDOW_WIDTH / 10.0f), ImGui::GetCursorPos().y + 0.15 * (WINDOW_HEIGHT / 10.0f)));
+				ImGui::SetCursorPos(ImVec2(ImGui::GetCursorPos().x + 1.8 * (WINDOW_WIDTH / 10.0f), ImGui::GetCursorPos().y + 0.15 * (WINDOW_HEIGHT / 10.0f)));
 				ImGui::Text("ENEL SP");
 				ImGui::SetWindowFontScale(1.4);
+				ImGui::SetCursorPos(ImVec2(ImGui::GetCursorPos().x + 1.5 * (WINDOW_WIDTH / 10.0f), ImGui::GetCursorPos().y + 0.3 * (WINDOW_HEIGHT / 10.0f)));
+				ImGui::Text("TUSD FIO B -->");
 				break;
 			case 1: // Rio Claro
 				// do something for Rio Claro in SP
 				ImGui::SetWindowFontScale(2.0);
-				ImGui::SetCursorPos(ImVec2(ImGui::GetCursorPos().x + 1.6 * (WINDOW_WIDTH / 10.0f), ImGui::GetCursorPos().y + 0.15 * (WINDOW_HEIGHT / 10.0f)));
+				ImGui::SetCursorPos(ImVec2(ImGui::GetCursorPos().x + 1.8 * (WINDOW_WIDTH / 10.0f), ImGui::GetCursorPos().y + 0.15 * (WINDOW_HEIGHT / 10.0f)));
 				ImGui::Text("ELEKTRO");
 				ImGui::SetWindowFontScale(1.4);
+				ImGui::SetCursorPos(ImVec2(ImGui::GetCursorPos().x + 1.5 * (WINDOW_WIDTH / 10.0f), ImGui::GetCursorPos().y + 0.3 * (WINDOW_HEIGHT / 10.0f)));
+				ImGui::Text("TUSD FIO B -->");
 				break;
 			case 2: // Campos de Jordao
 				// do something for Campos de Jordao in SP
 				ImGui::SetWindowFontScale(2.0);
-				ImGui::SetCursorPos(ImVec2(ImGui::GetCursorPos().x + 1.6 * (WINDOW_WIDTH / 10.0f), ImGui::GetCursorPos().y + 0.15 * (WINDOW_HEIGHT / 10.0f)));
+				ImGui::SetCursorPos(ImVec2(ImGui::GetCursorPos().x + 1.8 * (WINDOW_WIDTH / 10.0f), ImGui::GetCursorPos().y + 0.15 * (WINDOW_HEIGHT / 10.0f)));
 				ImGui::Text("ELEKTRO");
 				ImGui::SetWindowFontScale(1.4);
+				ImGui::SetCursorPos(ImVec2(ImGui::GetCursorPos().x + 1.5 * (WINDOW_WIDTH / 10.0f), ImGui::GetCursorPos().y + 0.3 * (WINDOW_HEIGHT / 10.0f)));
+				ImGui::Text("TUSD FIO B -->");
 
 				break;
 			case 3: // Adamantina
 				// do something for Adamantina in SP
 				ImGui::SetWindowFontScale(2.0);
-				ImGui::SetCursorPos(ImVec2(ImGui::GetCursorPos().x + 1.6 * (WINDOW_WIDTH / 10.0f), ImGui::GetCursorPos().y + 0.15 * (WINDOW_HEIGHT / 10.0f)));
+				ImGui::SetCursorPos(ImVec2(ImGui::GetCursorPos().x + 1.8 * (WINDOW_WIDTH / 10.0f), ImGui::GetCursorPos().y + 0.15 * (WINDOW_HEIGHT / 10.0f)));
 				ImGui::Text("ESS");
 				ImGui::SetWindowFontScale(1.4);
+				ImGui::SetCursorPos(ImVec2(ImGui::GetCursorPos().x + 1.5 * (WINDOW_WIDTH / 10.0f), ImGui::GetCursorPos().y + 0.3 * (WINDOW_HEIGHT / 10.0f)));
+				ImGui::Text("TUSD FIO B -->");
 				break;
 			}
 			break;
 		case 1: // TO
 			ImGui::Combo("City", &current_city, items_cities_TO, IM_ARRAYSIZE(items_cities_TO));
-			ImGui::SetCursorPos(ImVec2(ImGui::GetCursorPos().x + 1.6 * (WINDOW_WIDTH / 10.0f), ImGui::GetCursorPos().y - 1 * (WINDOW_HEIGHT / 10.0f)));
+			ImGui::SetCursorPos(ImVec2(ImGui::GetCursorPos().x + 1.8 * (WINDOW_WIDTH / 10.0f), ImGui::GetCursorPos().y - 1 * (WINDOW_HEIGHT / 10.0f)));
 			ImGui::Text("Power distribution \ncompany");
 			switch (current_city) {
 			case 0: // Palmas
 				// do something for Palmas in TO
 				ImGui::SetWindowFontScale(2.0);
-				ImGui::SetCursorPos(ImVec2(ImGui::GetCursorPos().x + 1.6 * (WINDOW_WIDTH / 10.0f), ImGui::GetCursorPos().y + 0.15 * (WINDOW_HEIGHT / 10.0f)));
+				ImGui::SetCursorPos(ImVec2(ImGui::GetCursorPos().x + 1.8 * (WINDOW_WIDTH / 10.0f), ImGui::GetCursorPos().y + 0.15 * (WINDOW_HEIGHT / 10.0f)));
 				ImGui::Text("ETO");
 				ImGui::SetWindowFontScale(1.4);
+				ImGui::SetCursorPos(ImVec2(ImGui::GetCursorPos().x + 1.5 * (WINDOW_WIDTH / 10.0f), ImGui::GetCursorPos().y + 0.3 * (WINDOW_HEIGHT / 10.0f)));
+				ImGui::Text("TUSD FIO B -->");
 				break;
 			case 1: // Gurupi
 				// do something for Gurupi in TO
@@ -122,11 +132,17 @@ int main()
 			break;
 		case 2: // GO
 			ImGui::Combo("City", &current_city, items_cities_GO, IM_ARRAYSIZE(items_cities_GO));
-			ImGui::SetCursorPos(ImVec2(ImGui::GetCursorPos().x + 1.6 * (WINDOW_WIDTH / 10.0f), ImGui::GetCursorPos().y - 1 * (WINDOW_HEIGHT / 10.0f)));
+			ImGui::SetCursorPos(ImVec2(ImGui::GetCursorPos().x + 1.8 * (WINDOW_WIDTH / 10.0f), ImGui::GetCursorPos().y - 1 * (WINDOW_HEIGHT / 10.0f)));
 			ImGui::Text("Power distribution \ncompany");
 			switch (current_city) {
 			case 0: // Goiania
 				// do something for Goiania in GO
+				ImGui::SetWindowFontScale(2.0);
+				ImGui::SetCursorPos(ImVec2(ImGui::GetCursorPos().x + 1.8 * (WINDOW_WIDTH / 10.0f), ImGui::GetCursorPos().y + 0.15 * (WINDOW_HEIGHT / 10.0f)));
+				ImGui::Text("ENEL GO");
+				ImGui::SetWindowFontScale(1.4);
+				ImGui::SetCursorPos(ImVec2(ImGui::GetCursorPos().x + 1.5 * (WINDOW_WIDTH / 10.0f), ImGui::GetCursorPos().y + 0.3 * (WINDOW_HEIGHT / 10.0f)));
+				ImGui::Text("TUSD FIO B -->");
 				break;
 			case 1: // Anapolis
 				// do something for Anapolis in GO
